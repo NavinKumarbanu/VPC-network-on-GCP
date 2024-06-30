1f8527bb1ce6bf7f01331a05b45e8adc23ede471
NavinKumarbanu <168159694+NavinKumarbanu@users.noreply.github.com>
2024-06-30 11:06:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,42 @@
+# provider block
 provider "google" {
   project = "alien-house-421716"
   region  = "us-central1"
 }
 
+# VPC network block
 resource "google_compute_network" "vpc_network" {
   name                    = "my-vpc-network"
   auto_create_subnetworks = false
 }
 
+# subnet within the VPC network block
 resource "google_compute_subnetwork" "subnetwork" {
   name          = "my-subnetwork"
   ip_cidr_range = "10.0.0.0/16"
   network       = google_compute_network.vpc_network.self_link
   region        = "us-central1"
 }
+
+# firewall rule to allow internal traffic
+resource "google_compute_firewall" "allow_internal" {
+  name    = "allow-internal"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports    = ["0-65535"]
+  }
+
+  source_ranges = ["10.0.0.0/16"]
+}
+
